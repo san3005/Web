33cd800daae93acb21e1959e39bd395b3287313b
san3005 <santhoshgurajada01@gmail.com>
2024-11-28 15:18:18 -0600

    added feedback from customers

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 const repoName = "web"; // Replace with your repository name
 
 module.exports = {
-  // output: "export",
-  // assetPrefix: isProd ? `/${repoName}/` : "",
-  // basePath: isProd ? `/${repoName}` : "",
+  output: "export",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  basePath: isProd ? `/${repoName}` : "",
 };
